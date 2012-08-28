a69bea7865c29b8bab8843adf1d741d200f4f090
James Slagle <slagle@redhat.com>
2012-08-28 15:26:17 -0400

    Set new templates dir path in settings.py

--- src/rhic_serve/rhic_serve/settings.py
+++ src/rhic_serve/rhic_serve/settings.py
@@ -132,7 +132,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    '/home/jslagle/code/splice/rhic-serve/src/rhic_serve/rhic_webui/templates',
+    '/usr/lib/rhic_webui/templates',
 )
 
 INSTALLED_APPS = (
