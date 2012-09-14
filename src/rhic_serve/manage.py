9c1590ea2307a016cefe800c6dcb1463c9e53282
James Slagle <slagle@redhat.com>
2012-09-14 15:05:50 -0400

    Move dev outside of src/

--- src/rhic_serve/manage.py
+++ src/rhic_serve/manage.py
@@ -4,6 +4,7 @@ import sys
 
 curr_dir = os.path.dirname(os.path.abspath(__file__))
 sys.path.insert(0, os.path.join(curr_dir, '..'))
+sys.path.insert(0, os.path.join(curr_dir, '../..'))
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dev.settings")
