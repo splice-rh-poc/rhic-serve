b94eadce3e5e0e5d3a887408c50c85106417aba3
James Slagle <slagle@redhat.com>
2012-09-17 17:34:13 -0400

    Add tests for rhic_rcs

--- src/rhic_serve/rhic_rcs/tests.py
+++ src/rhic_serve/rhic_rcs/tests.py
@@ -1,16 +1,37 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
+# -*- coding: utf-8 -*-
+#
+# Copyright © 2012 Red Hat, Inc.
+#
+# This software is licensed to you under the GNU General Public
+# License as published by the Free Software Foundation; either version
+# 2 of the License (GPLv2) or (at your option) any later version.
+# There is NO WARRANTY for this software, express or implied,
+# including the implied warranties of MERCHANTABILITY,
+# NON-INFRINGEMENT, or FITNESS FOR A PARTICULAR PURPOSE. You should
+# have received a copy of GPLv2 along with this software; if not, see
+# http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
 
-Replace this with more appropriate tests for your application.
-"""
 
-from django.test import TestCase
+import simplejson
+
+from rhic_serve.common.tests import MongoTestCase, MongoApiTestCase
 
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
+class RHICRcsApiTest(MongoApiTestCase):
+
+
+    def test_get_rhicsrcs(self):
         """
-        Tests that 1 + 1 always equals 2.
+        Get all rhics.
         """
-        self.assertEqual(1 + 1, 2)
+        response = self.get('/api/rhic/')
+        json = response.content
+        rhics = simplejson.loads(json)
+        self.assertEquals(8, len(rhics))
+
+    def test_get_rhicrcs(self):
+        response = self.get('/api/rhic/ed74e2a5-eb37-4bcb-9504-a9c338db56d0/')
+        json = response.content
+        rhic = simplejson.loads(json)
+        self.assertIsInstance(rhic, dict)
+        self.assertEquals('ed74e2a5-eb37-4bcb-9504-a9c338db56d0', rhic['uuid'])
