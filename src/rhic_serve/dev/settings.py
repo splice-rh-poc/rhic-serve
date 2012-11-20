d5b133e192543a122e18d3406eb9ab99dbf9fcdd
James Slagle <jslagle@redhat.com>
2012-11-20 15:58:15 -0500

    Update dev settings.py

--- src/rhic_serve/dev/settings.py
+++ src/rhic_serve/dev/settings.py
@@ -4,17 +4,22 @@ import os
 
 from splice.common import config
 
-SPLICE_CONFIG_FILE = 'dev/splice.conf'
-config.init(SPLICE_CONFIG_FILE)
+curr_dir = os.path.dirname(__file__)
+source_dir = os.path.join(curr_dir, '..')
+cert_dir = os.path.join(curr_dir, '../../..')
+os.chdir(source_dir)
+config_file = os.path.join(curr_dir, 'splice.conf')
+srl_path = os.path.join(cert_dir, 'etc/pki/rhic-serve/rhic-serve-ca.srl')
+
+# Reset configuration object just in case
+config.CONFIG = None
+
+config.init(config_file)
+
 
 from rhic_serve.settings import *
 
 TASTYPIE_FULL_DEBUG = True
-
-curr_dir = os.path.dirname(os.path.abspath(__file__))
-cert_dir = os.path.join(curr_dir, '../../..')
-
-srl_path = os.path.join(cert_dir, 'etc/pki/rhic-serve/rhic-serve-ca.srl')
 
 if not os.path.exists(srl_path):
     open(srl_path, 'w').write('01\n')
